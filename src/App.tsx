a9952f74e478710709cdeb2f4d007864ce4dbfe2
Matti Luukkainen <mluukkai@iki.fi>
2023-02-10 00:39:26 +0200

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Note } from "./types";
+import { getAllNotes, createNote } from './noteService';
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [newNote, setNewNote] = useState('');
+
+  useEffect(() => {
+    getAllNotes().then(data => {
+      setNotes(data)
+    })
+  }, [])
+
+  const noteCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    createNote({ content: newNote }).then(data => {
+      setNotes(notes.concat(data))
+    })
+
+    setNewNote('')
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={noteCreation}>
+        <input
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type='submit'>add</button>
+      </form>
+      <ul>
+        {notes.map(note => <li key={note.id}>
+          {note.content}
+        </li>)}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
